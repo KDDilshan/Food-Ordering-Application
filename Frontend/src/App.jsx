5890a1129ce370649fe3bc43ff1ee22b31a208a4
KDDilshan <kdprojects847@gmail.com>
2024-11-12 23:57:30 +0530

    added react router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/navbar/Navbar"
+import Home from "./pages/Home/Home"
+import Cart from "./pages/Cart/Cart"
 
 function App() {
 
   return (
     <div className="app">
       <Navbar />
+      <Routes >
+        <Route path="/" element={<Home /> } />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </div>
   )
 }
